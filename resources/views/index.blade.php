fc722a5123342de0febefa9f486dafa2a5028d87
Murat <>
2018-01-20 22:14:54 +0300

    compiling assets and adding index file for view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Music Player Application</title>
+
+    <link href="node_modules/all.css" rel="stylesheet">
+
+    <script src="node_modules/angular.min.js"></script>
+    <script src="node_modules/lodash.min.js"></script>
+    <script src="node_modules/angular-route.min.js"></script>
+    <script src="node_modules/angular-local-storage.min.js"></script>
+    <script src="node_modules/restangular.min.js"></script>
+
+    <style>
+
+        li {
+            padding-bottom: 8px;
+        }
+
+    </style>
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Music Player Application</h1>
+        </div>
+    </div>
+</div>
+
+<script src="node_modules/jquery.min.js"></script>
+<script src="node_modules/bootstrap.min.js"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,15 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.styles([
+    'node_modules/bootstrap/dist/css/bootstrap.min.css'
+], 'public/node_modules/all.css');
+
+mix.js('node_modules/angular/angular.min.js', 'node_modules/angular.min.js')
+    .js('node_modules/lodash/lodash.min.js', 'node_modules/lodash.min.js')
+    .js('node_modules/angular-route/angular-route.min.js', 'node_modules/angular-route.min.js')
+    .js('node_modules/angular-local-storage/dist/angular-local-storage.min.js', 'node_modules/angular-local-storage.min.js')
+    .js('node_modules/restangular/dist/restangular.min.js', 'node_modules/restangular.min.js')
+    .js('node_modules/jquery/dist/jquery.min.js', 'node_modules/jquery.min.js')
+    .js('node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/bootstrap.min.js');
